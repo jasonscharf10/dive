038856d74884700b35d581dcd2f0f19d1468e7e1
Jason Scharf <jason.scharf@pandadoc.com>
2024-04-09 10:28:08 -0400

    Add table and chart to client

--- client/main.py
+++ client/main.py
@@ -4,6 +4,8 @@ import asyncio
 import aiohttp
 
 import streamlit as st
+import pandas as pd
+import altair as alt
 
 async def main():
     """docstring"""
@@ -17,18 +19,18 @@ async def main():
                 async with session.get(url) as response:
                     data = await response.json()
                     st.title("First Streamlit Test App")
-                    st.write(data)
-                # chart_data = pd.DataFrame(
-                #     {
-                #         "title": [item["title"] for item in data],
-                #         "url": [item["url"] for item in data],
-                #         "published_date": [item["publishedAt"] for item in data],
-                #     }
-                # )
-                # st.write(chart_data)
-            # c = alt.Chart(chart_data).mark_bar().encode(x="url", y="count()")
-            # st.altair_chart(c, use_container_width=True)
-            # st.bar_chart(chart_data, x="url", y="name")
+                    #st.write(data)
+                    chart_data = pd.DataFrame(
+                        {
+                            "title": [item["title"] for item in data],
+                            "url": [item["url"] for item in data],
+                            "published_date": [item["published_date"] for item in data],
+                        }
+                    )
+                    st.write(chart_data)
+                    c = alt.Chart(chart_data).mark_bar().encode(x="published_date", y="count()")
+                    st.altair_chart(c, use_container_width=True)
+                    # st.bar_chart(chart_data, x="url", y="title")
 
 
 if __name__ == "__main__":
