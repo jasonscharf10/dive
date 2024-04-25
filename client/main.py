818ad48f8edb894c11397211d134496dd19cf206
Jason Scharf <jason.scharf@pandadoc.com>
2024-04-24 22:39:36 -0400

    Use streamlit sesion state

--- client/main.py
+++ client/main.py
@@ -9,45 +9,39 @@ import altair as alt
 import settings
 
 
+async def load_data(should_force_load: bool = False):
+    if should_force_load or "chart_data" not in st.session_state:
+        url = f"{settings.SERVER_API_BASE_URL}/fetch-data"
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                st.session_state["chart_data"] = pd.DataFrame(
+                    {
+                        "title": [item["title"] for item in data],
+                        "url": [item["url"] for item in data],
+                        "published_date": [item["published_date"] for item in data],
+                    }
+                )
+                print(st.session_state)
+
+
 async def main():
     """docstring"""
     st.title("PandaDoc News Articles")
-    url = f"{settings.SERVER_API_BASE_URL}/fetch-data"
-    # async with aiohttp.ClientSession(trust_env=True) as session:
-    #     async with session.get(url) as response:
-    #         data = await response.json()
-    #         chart_data = pd.DataFrame(
-    #             {
-    #                 "title": [item["title"] for item in data],
-    #                 "url": [item["url"] for item in data],
-    #                 "published_date": [item["published_date"] for item in data],
-    #             }
-    #         )
-    #         st.write(chart_data)
-    #         c = alt.Chart(chart_data).mark_bar().encode(x="published_date", y="count()")
-    #         st.altair_chart(c, use_container_width=True)
+    await load_data()
     if st.button("Refresh Data"):
         url = f"{settings.SERVER_API_BASE_URL}/update-data"
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.post(url) as response:
                 text = response.text
-                url = f"{settings.SERVER_API_BASE_URL}/fetch-data"
-                async with session.get(url) as response:
-                    data = await response.json()
-                    chart_data = pd.DataFrame(
-                        {
-                            "title": [item["title"] for item in data],
-                            "url": [item["url"] for item in data],
-                            "published_date": [item["published_date"] for item in data],
-                        }
-                    )
-                    st.write(chart_data)
-                    c = (
-                        alt.Chart(chart_data)
-                        .mark_bar()
-                        .encode(x="published_date", y="count()")
-                    )
-                    st.altair_chart(c, use_container_width=True)
+                await load_data(should_force_load=True)
+    st.write(st.session_state["chart_data"])
+    c = (
+        alt.Chart(st.session_state["chart_data"])
+        .mark_bar()
+        .encode(x="published_date", y="count()")
+    )
+    st.altair_chart(c, use_container_width=True)
 
 
 if __name__ == "__main__":
