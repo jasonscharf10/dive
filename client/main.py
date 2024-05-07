49d8746b794111bbb3e8db8acd9c0eaf56208e54
Jason Scharf <jason.scharf@pandadoc.com>
2024-05-07 11:50:13 -0400

    Add sentiment analysis

--- client/main.py
+++ client/main.py
@@ -8,6 +8,52 @@ import pandas as pd
 import altair as alt
 import settings
 
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+def preprocess_text(text):
+
+    # Tokenize the text
+
+    tokens = word_tokenize(text.lower())
+
+    # Remove stop words
+
+    filtered_tokens = [
+        token for token in tokens if token not in stopwords.words("english")
+    ]
+
+    # Lemmatize the tokens
+
+    lemmatizer = WordNetLemmatizer()
+
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    # Join the tokens back into a string
+
+    processed_text = " ".join(lemmatized_tokens)
+
+    return processed_text
+
+
+analyzer = SentimentIntensityAnalyzer()
+
+
+# create get_sentiment function
+
+
+def get_sentiment(text):
+
+    scores = analyzer.polarity_scores(text)
+
+    sentiment = scores["compound"]
+
+    return sentiment
+
 
 async def load_data(search_param, should_force_load: bool = False):
     if should_force_load or "chart_data" not in st.session_state:
@@ -26,11 +72,20 @@ async def load_data(search_param, should_force_load: bool = False):
                         "source": [item["source"] for item in data],
                     }
                 )
+                st.session_state["chart_data"].fillna("None", inplace=True)
+                st.session_state["chart_data"]["title"] = st.session_state[
+                    "chart_data"
+                ]["title"].apply(preprocess_text)
+                # apply get_sentiment function
+                st.session_state["chart_data"]["sentiment"] = st.session_state[
+                    "chart_data"
+                ]["title"].apply(get_sentiment)
 
 
 async def main():
     """docstring"""
-    st.title("Latest News Articles")
+    nltk.download("all")
+    st.title("News Articles Sentiment Analysis")
     search_param = st.text_input("Search parameter")
     await load_data(search_param)
     if st.button("Refresh Data"):
@@ -41,6 +96,8 @@ async def main():
             ) as response:
                 text = response.text
                 await load_data(search_param, should_force_load=True)
+    avg_sentiment = st.session_state["chart_data"].loc[:, "sentiment"].mean()
+    st.write("The Average Sentiment for " + str(search_param) + " is " + str(avg_sentiment))
     st.dataframe(
         st.session_state["chart_data"],
         use_container_width=True,
@@ -55,7 +112,7 @@ async def main():
     c = (
         alt.Chart(st.session_state["chart_data"])
         .mark_bar()
-        .encode(x="published_date", y="count()")
+        .encode(x="published_date", y="mean(sentiment)")
     )
     st.altair_chart(c, use_container_width=True)
 
