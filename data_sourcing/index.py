9f9a83b3982c118dd783a33e8d93510148899e80
Jason Scharf <jason.scharf@pandadoc.com>
2024-07-08 22:56:04 -0400

    Vercel attempt with aiohttp_asgi

--- data_sourcing/index.py
+++ data_sourcing/index.py
@@ -1,23 +1,25 @@
 import asyncio
-import asyncpg
 from aiohttp import web
 import settings
 from workers.news_api import news_api_worker
 from workers.reddit_api import reddit_api_worker
+from aiohttp_asgi import ASGIApplication
 
 async def run_tasks(request):
-    tasks = []
-    tasks.append(asyncio.create_task(news_api_worker()))
-    tasks.append(asyncio.create_task(reddit_api_worker()))
-
+    tasks = [
+        asyncio.create_task(news_api_worker()),
+        asyncio.create_task(reddit_api_worker())
+    ]
     await asyncio.gather(*tasks)
-
     return web.Response(text="Tasks completed", status=200)
 
 app = web.Application()
 app.add_routes([web.get('/', run_tasks)])
 
+# Wrap the aiohttp app with aiohttp-asgi for ASGI compatibility
+asgi_app = ASGIApplication(app)
+
 if __name__ == "__main__":
     web.run_app(app)
 else:
-    asgi_app = app
+    app = asgi_app
