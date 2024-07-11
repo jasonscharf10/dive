41f436495ffc0ed5c3fb6eeb65c3e443ff53f348
Jason Scharf <jason.scharf@pandadoc.com>
2024-07-11 10:41:54 -0400

    Update index.py in data_sourcing

--- data_sourcing/index.py
+++ data_sourcing/index.py
@@ -1,22 +1,13 @@
 import asyncio
-from aiohttp import web
-import settings
 from workers.news_api import news_api_worker
 from workers.reddit_api import reddit_api_worker
 
-async def run_tasks(request):
+async def main():
     tasks = [
         asyncio.create_task(news_api_worker()),
         asyncio.create_task(reddit_api_worker())
     ]
     await asyncio.gather(*tasks)
-    return web.Response(text="Tasks completed", status=200)
-
-app = web.Application()
-app.add_routes([web.get('/', run_tasks)])
 
 if __name__ == "__main__":
-    web.run_app(app)
-else:
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    asyncio.run(main())
