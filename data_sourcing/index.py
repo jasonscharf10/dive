5a6f22d316050d28fd4aa1e6860f1beb897d985c
Jason Scharf <jason.scharf@pandadoc.com>
2024-07-11 10:47:57 -0400

    Add app to index.py

--- data_sourcing/index.py
+++ data_sourcing/index.py
@@ -1,13 +1,19 @@
 import asyncio
+from aiohttp import web
 from workers.news_api import news_api_worker
 from workers.reddit_api import reddit_api_worker
 
-async def main():
+async def run_tasks(request):
     tasks = [
         asyncio.create_task(news_api_worker()),
         asyncio.create_task(reddit_api_worker())
     ]
     await asyncio.gather(*tasks)
+    return web.Response(text="Tasks started")
 
+app = web.Application()
+app.router.add_get("/", run_tasks)
+
+# Make the app visible to Vercel
 if __name__ == "__main__":
-    asyncio.run(main())
+    web.run_app(app)
