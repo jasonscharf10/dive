100445e0eee4e6ab1b2d80eae35e8a6133289f91
Jason Scharf <jason.scharf@pandadoc.com>
2024-07-16 09:41:22 -0400

    Create new endpoint for fetch_user_input

--- server/main.py
+++ server/main.py
@@ -3,9 +3,10 @@ import asyncio
 from aiohttp import web
 
 from init_db import Database
+from handlers.save_data import save_data
 from handlers.fetch_data import fetch_data
 from handlers.save_user_input import save_user_input
-from handlers.save_data import save_data
+from handlers.fetch_user_input import fetch_user_input
 
 
 async def main():
@@ -20,6 +21,7 @@ app.add_routes(
         web.post("/save-data", save_data),
         web.get("/fetch-data", fetch_data),
         web.post("/save-user-input", save_user_input),
+        web.get("/fetch-user-input", fetch_user_input),
     ]
 )
 
